78e2eba5c994197000af64f456626a96b7a5a34b
alfajri23 <123190088@student.upnyk.ac.id>
2022-03-20 21:38:28 +0700

    eslint & gitignore

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const expressLayout = require('express-ejs-layouts');
+const express = require("express");
 
-const routes = require('./routes/route.js');
+const app = express();
+const port = 3000;
+const expressLayout = require("express-ejs-layouts");
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs');
+const routes = require("./routes/route.js");
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.use(expressLayout);
 
 routes(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
